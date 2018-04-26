8f39a285373d59350c28c6c12c21cf7224c87e7d
xiaojun.zhang1 <xiaojun.zhang1@china.zhaogang.com>
2018-04-26 14:14:31 +0800

    上传配置文件

---
+++ configserverr/src/main/java/com/zxj/spring/ConfigServerApplication.java
@@ -0,0 +1,21 @@
+package com.zxj.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Description:
+ * @Author:zhangxiaojun
+ * @Date:Created in 14:06 2018/4/26
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
