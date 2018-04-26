24b042d2edad7039c64028592f5cfce070b63487
xiaojun.zhang1 <xiaojun.zhang1@china.zhaogang.com>
2018-04-26 13:52:13 +0800

    Initial commit

---
+++ eurekaserver/src/main/java/com/zxj/spring/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.zxj.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Description:
+ * @Author:zhangxiaojun
+ * @Date:Created in 10:36 2018/4/26
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
